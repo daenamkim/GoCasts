422ab9826f671ac0b10387d8c16c3952ee742cbb
Daenam Kim <daenam.kim@gmail.com>
2019-01-31 09:53:59 +0900

    Add more receivers and functions

--- code/brush-up/cards/deck.go
+++ code/brush-up/cards/deck.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
-// Create a new type of 'deck'
-// which is a slice of strings
 type deck []string
 
 func newDeck() deck {
@@ -25,4 +28,26 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	return deck(strings.Split(string(bs), ","))
 }
