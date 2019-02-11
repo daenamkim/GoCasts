b2e55d44e4cf78742b43889ae449b2975cd5d8df
Daenam Kim <daenam.kim@gmail.com>
2019-02-11 23:46:18 +0900

    basic go routine and channel

---
+++ code/brush-up/channels/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://stackoverflow.com",
+		"https://golang.com",
+		"https://amazon.com",
+		"https://oootoko.net",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- "Yep it is up"
+}
