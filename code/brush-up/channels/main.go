3b892956693e1b04028f6181acb457f14fe2a521
Daenam Kim <daenam.kim@gmail.com>
2019-02-12 00:13:33 +0900

    pause in channels

--- code/brush-up/channels/main.go
+++ code/brush-up/channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -21,19 +22,26 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	// simple
+	// for {
+	// 	go checkLink(<-c, c)
+	// }
+
+	// clearer (channel is blocking)
+	for l := range c {
+		go checkLink(l, c)
 	}
 }
 
 func checkLink(link string, c chan string) {
+	time.Sleep(5 * time.Second)
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep it is up"
+	c <- link
 }
